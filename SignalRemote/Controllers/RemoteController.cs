7ac78ac4e6358a01108c985d41830aaadbad78b0
William Wilson <w.wilson1@gmail.com>
2015-01-28 22:15:49 -0500

    changes image format to lower quality jpeg and requests next screen update only after loading (with rate limiter)

--- SignalRemote/Controllers/RemoteController.cs
+++ SignalRemote/Controllers/RemoteController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace SignalRemote.Controllers
 {
   public class RemoteController : Controller
   {
+    private static object jpegCodecLock = new object();
+    private static ImageCodecInfo jpegCodec = null;
+
     public ActionResult Index()
     {
       return View();
@@ -17,11 +21,30 @@ namespace SignalRemote.Controllers
 
       using (var image = new ScreenCapture().GetScreenshot())
       {
-        image.Save(imageBytes, ImageFormat.Png);
+        var codec = GetJpegCodec();
+        var codecParams = new EncoderParameters();
+        codecParams.Param[0] = new EncoderParameter(Encoder.Quality, (long)50);
+        image.Save(imageBytes, codec, codecParams);
       }
 
       imageBytes.Seek(0, SeekOrigin.Begin);
-      return new FileStreamResult(imageBytes, "image/png");
+      return new FileStreamResult(imageBytes, "image/jpeg");
+    }
+
+    private ImageCodecInfo GetJpegCodec()
+    {
+      if (jpegCodec == null)
+      {
+        lock (jpegCodecLock)
+        {
+          if (jpegCodec == null)
+          {
+            jpegCodec = ImageCodecInfo.GetImageEncoders().FirstOrDefault(c => c.FormatID == ImageFormat.Jpeg.Guid);
+          }
+        }
+      }
+
+      return jpegCodec;
     }
   }
 }
